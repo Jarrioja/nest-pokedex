82f2254e472880cc13e35da0ff350203ace48027
Jesus Arrioja <jarrioja2210@gmail.com>
2024-08-24 16:07:12 -0300

    initial commit

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,10 @@
+import { IsNumber, IsPositive, IsString, Min } from 'class-validator';
+export class CreatePokemonDto {
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  name: string;
+}
